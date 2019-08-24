8af3f2833c126f295853c45f31ad20d2b0f6026c
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2019-08-24 20:05:06 +0200

    adding logic begining

--- src/views/game.vue
+++ src/views/game.vue
@@ -5,16 +5,33 @@
 </template>
 <script>
 export default {
+  data: function () {
+    return {
+      numberOfRound: 0
+    }
+  },
   methods: {
-    hello () {
-      console.log('hey')
-    },
-    giveCards () {
 
+    giveCards () {
+      const cards = ['7 coeur', '8 coeur', '9 coeur', '10 coeur', 'V coeur', 'D coeur', 'R coeur', 'A coeur',
+        '7 pic', '8 pic', '9 pic', '10 pic', 'V pic', 'D pic', 'R pic', 'A pic',
+        '7 carreau', '8 carreau', '9 carreau', '10 carreau', 'V carreau', 'D carreau', 'R carreau', 'A carreau',
+        '7 trèfle', '8 trèfle', '9 trèfle', '10 trèfle', 'V trèfle', 'D trèfle', 'R trèfle', 'A trèfle']
+      console.log(cards)
+      randomCards()
+
+      function randomCards () {
+        let randomNumber = Math.round(Math.random() * cards.length)
+        let randomCard = cards[randomNumber]
+        console.log(randomNumber)
+        console.log(randomCard)
+        cards.splice(cards.indexOf(randomCard), 1)
+        console.log(cards)
+      }
     }
   },
   mounted () {
-    this.hello()
+    this.giveCards()
   }
 }
 </script>
