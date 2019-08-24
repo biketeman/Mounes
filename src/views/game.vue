91a1d66fb65a519d987340e2222abaf42faa1b31
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2019-08-24 19:30:33 +0200

    mise en place

---
+++ src/views/game.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>hey!</h1>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    hello () {
+      console.log('hey')
+    },
+    giveCards () {
+
+    }
+  },
+  mounted () {
+    this.hello()
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
